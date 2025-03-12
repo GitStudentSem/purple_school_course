1703293e7d69b7d9c927eda8b635e19386b93655
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2025-03-12 19:39:33 +0300

    validation for id param

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -14,6 +14,7 @@ import { Product } from "./product.model";
 import { FindProductDto } from "./dto/find-product.dto";
 import { CreateProductDto } from "./dto/createProduct.dto";
 import { ProductService } from "./product.service";
+import { IdValidationPipe } from "@src/pipes/id-validation.pipe";
 
 @Controller("product")
 export class ProductController {
@@ -25,17 +26,17 @@ export class ProductController {
 	}
 
 	@Get(":id")
-	async get(@Param("id") id: string) {
+	async get(@Param("id", IdValidationPipe) id: string) {
 		return this.productService.findById(id);
 	}
 
 	@Delete(":id")
-	async delete(@Param("id") id: string) {
+	async delete(@Param("id", IdValidationPipe) id: string) {
 		return this.productService.deleteById(id);
 	}
 
 	@Patch(":id")
-	async patch(@Param("id") id: string, @Body() dto: Product) {
+	async patch(@Param("id", IdValidationPipe) id: string, @Body() dto: Product) {
 		return this.productService.updateById(id, dto);
 	}
 
