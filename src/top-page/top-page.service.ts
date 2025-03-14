ca5f6352eea5f4f2d18dcb65bbe13776fec29c9f
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2025-03-14 18:15:10 +0300

    group and push in momgo

--- src/top-page/top-page.service.ts
+++ src/top-page/top-page.service.ts
@@ -60,9 +60,18 @@ export class TopPageService {
 
 	async findByCategory(firstCategory: TopLevelCategory) {
 		return await this.topPageModel
-			.find({ firstCategory }, { alias: 1, secondCategory: 1, title: 1 })
+			// .find({ firstCategory }, { alias: 1, secondCategory: 1, title: 1 })
+			.aggregate()
+			.match({ firstCategory })
+			.group({
+				_id: {
+					secondCategory: "$secondCategory",
+				},
+				pages: { $push: { alias: "$alias", title: "$title" } },
+			})
 			.exec();
 	}
+
 	async findByText(text: string) {
 		return this.topPageModel
 			.find({ $text: { $search: text, $caseSensitive: false } })
